af46be5dc5fc03ec6dff15acfeb728e3fba3a676
Jakub Chotětický <contact@aragocz.com>
2024-05-07 09:25:45 +0000

    update

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ require('dotenv').config({
     path:  __dirname+'/.env'
 });
 
-fs.createReadStream(__dirname+"/mod/"+fs.readdirSync(__dirname+"/mod")[0]).pipe(unzip.Extract({path: __dirname+"/modextracted/"}));
+const zip = fs.readdirSync(__dirname+"/mod")[0];
+//fs.createReadStream(__dirname+"/mod/"+fs.readdirSync(__dirname+"/mod")[0]).pipe(unzip.Extract({path: __dirname+"/modextracted/"}));
+fs.createReadStream(__dirname+"/mod/"+zip)
+    .pipe(unzip.Parse())
+    .on("entry", (entry) => {
+        const fileName = entry.path;
+        const fileType = entry.type;
+        //entry.pipe(fs.createWriteStream(__dirname+"/modsextracted"))
+        if(fileType === "File" && fileName.startsWith(zip.split(".zip")[0]+"/mods/")){
+            entry.pipe(fs.createWriteStream(__dirname+"/mods/"+fileName.split(zip.split(".zip")[0]+"/mods/").pop()));
+        }else if(fileType === "File" && fileName.startsWith(zip.split(".zip")[0]+"/res_mods/")){
+            entry.pipe(fs.createWriteStream(__dirname+"/res_mods/"))
+        }else entry.autodrain();
+    });
 
 //fs.rmSync(__dirname+"/mod/"+fs.readdirSync(__dirname+"/mod")[0]);
 //fs.renameSync(__dirname+"/mod/"+fs.readdirSync(__dirname+"/mod")[0], process.env.wotPath+"/mod.zip");
