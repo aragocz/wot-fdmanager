f4fe9edf64b3d32f5a694686f5f17c6ae6891b4c
aragocz <kubuleulule@email.cz>
2024-05-10 01:07:20 +0200

    Finish

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const fs = require("fs");
 const path = require("path")
 const unzip = require("unzipper");
+const execdirname = path.dirname(process.execPath);
 
 require('dotenv').config({
-    path:  __dirname+'/.env'
+    path:  execdirname+'/.env'
 });
 
-const zipname = fs.readdirSync(path.join(__dirname, "/mod/"))[0].split(".zip")[0] + "\\";
+const zipname = fs.readdirSync(path.join(execdirname, "/mod/"))[0].split(".zip")[0] + "\\";
 
 if(fs.existsSync(path.join(process.env.wotPath, "/mods")))fs.rmSync(path.join(process.env.wotPath, "/mods"), {recursive:true})
 if(fs.existsSync(path.join(process.env.wotPath, "/res_mods")))fs.rmSync(path.join(process.env.wotPath, "/res_mods"), {recursive:true})
 
-fs.createReadStream(path.join(__dirname, "/mod/", fs.readdirSync(path.join(__dirname,"/mod/"))[0])).pipe(unzip.Extract({path: path.join(__dirname,"/modextracted")}))
+fs.createReadStream(path.join(execdirname, "/mod/", fs.readdirSync(path.join(execdirname,"/mod/"))[0])).pipe(unzip.Extract({path: path.join(execdirname,"/modextracted")}))
     .once("close", (e) => {
-        fs.renameSync(path.join(__dirname, "/modextracted/", zipname, "mods"), path.join(process.env.wotPath, "/mods"));
-        fs.renameSync(path.join(__dirname, "/modextracted/", zipname, "res_mods"), path.join(process.env.wotPath, "/res_mods"));
+        fs.renameSync(path.join(execdirname, "/modextracted/", zipname, "mods"), path.join(process.env.wotPath, "/mods"));
+        fs.renameSync(path.join(execdirname, "/modextracted/", zipname, "res_mods"), path.join(process.env.wotPath, "/res_mods"));
     })
     .once("close", (e) => {
-        fs.rmSync(path.join(__dirname+"/mod/", fs.readdirSync(__dirname+"/mod")[0]));
-        fs.rmSync(path.join(__dirname+"/modextracted/", fs.readdirSync(__dirname+"/modextracted")[0]), {recursive: true});
+        fs.rmSync(path.join(execdirname+"/mod/", fs.readdirSync(execdirname+"/mod")[0]));
+        fs.rmSync(path.join(execdirname+"/modextracted/", fs.readdirSync(execdirname+"/modextracted")[0]), {recursive: true});
     })
 
 
