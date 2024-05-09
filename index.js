c73b369eb4dcbc5b54391bc0f3392bf062bf6cc4
aragocz <kubuleulule@email.cz>
2024-05-10 00:47:27 +0200

    Finishing touch of the alternate

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//UNFINISHED VERSION, DOES NOT WORK
-
 const fs = require("fs");
 const path = require("path")
 const unzip = require("unzipper");
@@ -8,28 +6,19 @@ require('dotenv').config({
     path:  __dirname+'/.env'
 });
 
-const zip = fs.readdirSync(__dirname+"/mod")[0];
-//fs.createReadStream(__dirname+"/mod/"+fs.readdirSync(__dirname+"/mod")[0]).pipe(unzip.Extract({path: __dirname+"/modextracted/"}));
-fs.createReadStream(__dirname+"/mod/"+zip)
-    .pipe(unzip.Parse())
-    .on("entry", (entry) => {
-        const fileName = entry.path;
-        const fileType = entry.type;
-        //entry.pipe(fs.createWriteStream(__dirname+"/modsextracted"))
-        if(fileName === path.join(zip, "/mods/")){
-            entry.autodrain();
-        }
-        if(fileType === "File" && fileName.startsWith(zip.split(".zip")[0]+"/mods/")){
-            //entry.pipe(fs.createWriteStream(path.join(__dirname,"/mods/",fileName.split(zip.split(".zip")[0]).pop())));
-        }else if(fileType === "File" && fileName.startsWith(zip.split(".zip")[0]+"/res_mods/")){
-            //entry.pipe(fs.createWriteStream(__dirname+"/res_mods/"))
-        }else if(fileType === "Directory" && fileName.startsWith(zip.split(".zip")[0]+"/mods/")){
-            console.log(fileName/*.split(zip.split(".zip").pop())*/)
-            console.log(path.join(__dirname,"/mods/",fileName.split(zip.split(".zip")[0])))
-            //entry.pipe(fs.mkdirSync(__dirname+"/mods/"+fileName.split(zip.split(".zip"))))
-            
-        }
-    });
+const zipname = fs.readdirSync(path.join(__dirname, "/mod/"))[0].split(".zip")[0] + "\\";
 
-//fs.rmSync(__dirname+"/mod/"+fs.readdirSync(__dirname+"/mod")[0]); entry.autodrain();
-//fs.renameSync(__dirname+"/mod/"+fs.readdirSync(__dirname+"/mod")[0], process.env.wotPath+"/mod.zip");
+if(fs.existsSync(path.join(process.env.wotPath, "/mods")))fs.rmSync(path.join(process.env.wotPath, "/mods"), {recursive:true})
+if(fs.existsSync(path.join(process.env.wotPath, "/res_mods")))fs.rmSync(path.join(process.env.wotPath, "/res_mods"), {recursive:true})
+
+fs.createReadStream(path.join(__dirname, "/mod/", fs.readdirSync(path.join(__dirname,"/mod/"))[0])).pipe(unzip.Extract({path: path.join(__dirname,"/modextracted")}))
+    .once("close", (e) => {
+        fs.renameSync(path.join(__dirname, "/modextracted/", zipname, "mods"), path.join(process.env.wotPath, "/mods"));
+        fs.renameSync(path.join(__dirname, "/modextracted/", zipname, "res_mods"), path.join(process.env.wotPath, "/res_mods"));
+    })
+    .once("close", (e) => {
+        fs.rmSync(path.join(__dirname+"/mod/", fs.readdirSync(__dirname+"/mod")[0]));
+        fs.rmSync(path.join(__dirname+"/modextracted/", fs.readdirSync(__dirname+"/modextracted")[0]), {recursive: true});
+    })
+
+
