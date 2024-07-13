f189104ef465c2129118fc2caf5af936618cd305
aragocz <kubuleulule@email.cz>
2024-07-13 19:15:53 +0200

    Added automatic download of mod archive

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const fs = require("fs");
 const path = require("path")
 const unzip = require("unzipper");
+const axios = require("axios");
+const { JSDOM } = require("jsdom");
 const execdirname = path.dirname(process.execPath);
 
 require('dotenv').config({
     path:  execdirname+'/.env'
+});
+
+axios.get("https://www.frost-zone.eu/fdmod/").then((res) => {
+    const doc = new JSDOM(res.data);
+    const link = doc.window.document.querySelector("div.vc_btn3-container a.vc_btn3").href
+
+    axios({
+        method: "get",
+        url: link,
+        responseType: "stream"
+    }).then((res) => {
+        res.data.pipe(fs.createWriteStream("./mod/"+link.split("/").pop()))
+    })
 });
 
 const zipname = fs.readdirSync(path.join(execdirname, "/mod/"))[0].split(".zip")[0] + "\\";
